18f168612b853167854e84f5c213888a86d5e414
Vitalij Nykyforenko <vitalij.nykyforenko@gmail.com>
2019-10-05 15:49:57 +0300

    add canvas

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-console.log('hello');
-console.log('hello');
+const root: HTMLElement = document.getElementById('root');
+const canvas: HTMLCanvasElement = document.createElement('canvas');
+
+canvas.width = 600;
+canvas.height = 600;
+
+root.appendChild(canvas);
+
+const context: CanvasRenderingContext2D = canvas.getContext('2d');
+
